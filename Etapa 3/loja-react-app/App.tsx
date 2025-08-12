ce9027ceef06d39fd870913ef5e8f242d754976c
Lívia Pires <livia.pires@aluno.ifsp.edu.br>
2025-08-12 08:43:11 -0300

    Inclui ThemeContext

--- Etapa 3/loja-react-app/App.tsx
+++ Etapa 3/loja-react-app/App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { ThemeProvider } from './scr/contexts/ThemeContext';
+
 import HomeScreen from './scr/screens/HomeScreen';
 
 export default function App() {
   return (
-    <HomeScreen/>
+    <ThemeProvider>
+      <HomeScreen />
+    </ThemeProvider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
